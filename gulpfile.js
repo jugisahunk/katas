dc89d8205565e5e6d9b0cebf2ee05fd9bdb145af
Jason Knight <jugisahunk@gmail.com>
2015-11-04 17:13:28 -0600

    reworked placement of gulpfile and package.json to facilitate easier setup

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,10 @@ var gulp = require('gulp');
 var jasmine = require('gulp-jasmine');
 
 gulp.task('bowling-kata', function(){
-  gulp.watch(['bowling/**/*.js'], ['bowling-test']);
+  gulp.watch(['./dojo/sensei/bowling/**/*.js'], ['bowling-test']);
 });
 
 gulp.task('bowling-test', function(){
-  return gulp.src('bowling/*-test.js')
+  return gulp.src('./dojo/sensei/bowling/*-test.js')
     .pipe(jasmine({verbose:true, includeStackTrace: false}));
 });
